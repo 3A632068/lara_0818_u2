65f1f62ed8ee366ff73af6e62891f95cb8232d6c
PengHueiSin <eason12326@gmail.com>
2019-10-29 16:54:36 +0800

    練習 2-3:建立共用區塊

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,8 @@
     <title>@yield('title')|MyBlog</title>
     </head>
     <body>
+    <!-- 練習 2-3:建立共用區塊-->
+    @include('layouts.partials.navigation')
     <div class="container">
         @yield('content')
     </div>
