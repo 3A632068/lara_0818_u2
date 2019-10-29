ceca1b59219a7db937af208142edd0689ee2a50e
PengHueiSin <eason12326@gmail.com>
2019-10-29 17:00:12 +0800

    練習 3-4:從 Controller 將參數傳遞至 view

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    //練習 3-4:從 Controller 將參數傳遞至 view
+    public function index($name='Everybody')
+    {
+        $data=['name'=>$name];
+        return view('hello.index',$data);
+    }
 }
