039f9dcb166b12864f7b972edbdf39ceb5851521
PengHueiSin <eason12326@gmail.com>
2019-10-29 16:43:50 +0800

    修改註解

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,11 @@ Route::get('/', function () {
 });
 
 //練習 1-2:設定從 app/Http/routes.php回傳home.php頁面
-Route::get('home',function(){
+/*Route::get('home',function(){
     return view('home');
+});*/
+
+//練習 1-6:修改 app/Http/routes.php載入的頁面路徑
+Route::get('home',function(){
+    return view('home.index');
 });
